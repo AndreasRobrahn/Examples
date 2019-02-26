117ea4607f0b2f3c384c85a6e134bdacdc091b20
AndreasRobrahn <40436788+AndreasRobrahn@users.noreply.github.com>
2019-02-26 16:41:19 +0100

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// Create a file config.php and insert your database specifics
+return [
+    "server"=>"ANDI-PC\SQLEXPRESS",
+    
+    "database" => array("Database"=> "LagerDatenbank",
+					"ReturnDatesAsStrings"=> true)
+];
+?>
