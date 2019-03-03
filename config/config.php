de45a97ad7c53832895c8286cea47f6e239241ff
AndreasRobrahn <40436788+AndreasRobrahn@users.noreply.github.com>
2019-03-03 13:09:51 +0100

    Delete config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Create a file config.php and insert your database specifics
-return [
-    "server"=>"ANDI-PC\SQLEXPRESS",
-    
-    "database" => array("Database"=> "LagerDatenbank",
-					"ReturnDatesAsStrings"=> true)
-];
-?>
